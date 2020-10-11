3d7ff6d46738e13fd32093ce939f78edd551a0d6
mkerimbay <maxat.kerimbayev@gmail.com>
2020-10-11 16:55:51 +0600

    requirements

--- daily.py
+++
@@ -1,8 +0,0 @@
-import os
-from helper import logger
-from _credentials import *
-abspath = os.path.abspath(__file__)
-curr_path = os.path.dirname(abspath)
-os.chdir(curr_path)
-#logger.info('hello world')
-print(key)
